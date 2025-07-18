7ba9af6e3d306911f761055d2ba1871d6fa5c29b
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2025-07-18 21:38:06 +0530

    Add files via upload

---
+++ staticmethod.java
@@ -0,0 +1,11 @@
+public class staticmethod {
+    public static void main(String[] args) {
+        class car{
+            static void sound(){
+                System.out.println("car start");
+            }
+        }
+        car.sound();
+    }
+    
+}
