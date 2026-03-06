c1b9c1d7bb3f7c126147809bb0f728996b9df2bb
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-06 11:39:45 +0530

    Add files via upload

---
+++ patterns2.java
@@ -0,0 +1,18 @@
+public class patterns2 {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=0;i<n;i++){
+            System.out.println("fuck");
+        
+            for(int j=0;j<n;j++){
+                System.out.println("*");
+                //System.out.println();
+            }
+            System.out.println("raa");
+        }
+        
+            //System.out.println();
+    }
+        
+}
+    
