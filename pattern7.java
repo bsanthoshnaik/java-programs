6ea246709e80104de6e4f4db9b83612c50e3ae84
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-06 11:41:50 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,15 @@
+public class pattern7 {
+    public static void main(String []args){
+        int n=3;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=0;k<n;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
