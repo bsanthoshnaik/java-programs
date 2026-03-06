d7d8d42caa72db8fcc66581900c40c9bb50ed253
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-06 11:41:02 +0530

    Add files via upload

---
+++ patterns5.java
@@ -0,0 +1,18 @@
+public class patterns5 {
+    public static void main(String[] args){
+        int n=5hg;
+        int m=9;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 ||j==0 ||i==n-1 ||j==m-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
