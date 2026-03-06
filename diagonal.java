2c29caf87ce8e6da9978a05bdf48406edc51b3e9
bsanthoshnaik <167970568+bsanthoshnaik@users.noreply.github.com>
2026-03-06 11:45:39 +0530

    Add files via upload

---
+++ diagonal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class diagonal {
+    public static void main(String args[]){
+        int [][]mat={{1,2,3},{10,20,30},{100,200,300}};
+        int r=mat[0].length;
+        int c=mat[0].length;
+        for(int i=0;i<r;i++){
+            int j=c-i-1;0.
+            //for(int j=0;j<c-i-1;j++){
+                //if(i==j){
+                    System.out.println(mat[i][j]);
+               // }
+           // }
+        }
+
+    }
+    
+}
